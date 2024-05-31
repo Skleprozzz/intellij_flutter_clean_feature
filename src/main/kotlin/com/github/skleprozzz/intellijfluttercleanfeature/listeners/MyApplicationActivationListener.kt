41f2bedce3317b2ebea11c181877157d2920b040
GitHub Action <action@github.com>
2024-05-31 19:05:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/skleprozzz/intellijfluttercleanfeature/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/skleprozzz/intellijfluttercleanfeature/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.skleprozzz.intellijfluttercleanfeature.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
